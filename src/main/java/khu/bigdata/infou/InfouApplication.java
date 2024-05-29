ee8cd350bc351d472644c0960ec06d23e3868450
yxhwxn <officeyh9819@gmail.com>
2024-05-29 22:30:19 +0900

    Init project

---
+++ src/main/java/khu/bigdata/infou/InfouApplication.java
@@ -0,0 +1,13 @@
+package khu.bigdata.infou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/khu/bigdata/infou/InfouApplicationTests.java
@@ -0,0 +1,13 @@
+package khu.bigdata.infou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
