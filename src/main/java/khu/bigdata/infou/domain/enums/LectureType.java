e9079f02b92af1e3ce3f790936b8d6d0a1955378
yxhwxn <officeyh9819@gmail.com>
2024-06-08 23:45:54 +0900

    Feat: enum 엔티티 클래스 추가

---
+++ src/main/java/khu/bigdata/infou/domain/enums/LectureType.java
@@ -0,0 +1,5 @@
+package khu.bigdata.infou.domain.enums;
+
+public enum LectureType {
+    INFLEARN, UDEMY
+}
---
+++ src/main/java/khu/bigdata/infou/domain/enums/UserType.java
@@ -0,0 +1,5 @@
+package khu.bigdata.infou.domain.enums;
+
+public enum UserType {
+    INFLEARN, UDEMY
+}
