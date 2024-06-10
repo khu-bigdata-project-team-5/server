bea4d29f717ac7a8201855a7e9e0e69ac81d72d9
yxhwxn <officeyh9819@gmail.com>
2024-06-11 00:17:25 +0900

    :recycle: Refactor: swagger description 수정

--- src/main/java/khu/bigdata/infou/web/controller/MainServiceController.java
+++ src/main/java/khu/bigdata/infou/web/controller/MainServiceController.java
@@ -17,7 +17,7 @@ public class MainServiceController {
     private final LectureService lectureService;
 
     @GetMapping("/lectures/categories")
-    @Operation(summary = "카테고리별 추천 강좌 조회 API", description = "카테고리: Programming Languages, Web Development, Game Development, Mobile Development, Data Science")
+    @Operation(summary = "카테고리별 추천 강좌 조회 API", description = "카테고리: Programming Language, Web Development, Game Development, Mobile Development, Data Science")
     public ResponseEntity<LectureResponseDTO.CategoryRecommendLectureDto> getRecommendedLectureByCategory(
             @RequestParam(name = "category") String category
     ) {
