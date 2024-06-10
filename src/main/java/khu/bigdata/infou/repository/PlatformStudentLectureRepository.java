691242d8d439f801a235dd0c6219c9d84720cc58
yxhwxn <officeyh9819@gmail.com>
2024-06-10 22:32:41 +0900

    :sparkles: Feat: Udemy, Inflearn 유저 모델 관련 repository 추가

---
+++ src/main/java/khu/bigdata/infou/repository/PlatformStudentLectureRepository.java
@@ -0,0 +1,7 @@
+package khu.bigdata.infou.repository;
+
+import khu.bigdata.infou.domain.PlatformStudentLecture;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlatformStudentLectureRepository extends JpaRepository<PlatformStudentLecture, Long> {
+}
---
+++ src/main/java/khu/bigdata/infou/repository/PlatformStudentRepository.java
@@ -0,0 +1,7 @@
+package khu.bigdata.infou.repository;
+
+import khu.bigdata.infou.domain.PlatformStudent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlatformStudentRepository extends JpaRepository<PlatformStudent, Long> {
+}
