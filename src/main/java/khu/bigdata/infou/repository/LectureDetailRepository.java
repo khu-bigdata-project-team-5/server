73de8d7f3a343b6cf06fc645890c38c925ea912a
yxhwxn <officeyh9819@gmail.com>
2024-06-10 21:27:45 +0900

    :sparkles: Feat: 강좌 세부 조회 API 개발

--- src/main/java/khu/bigdata/infou/repository/LectureDetailRepository.java
+++ src/main/java/khu/bigdata/infou/repository/LectureDetailRepository.java
@@ -1,7 +1,12 @@
 package khu.bigdata.infou.repository;
 
 import khu.bigdata.infou.domain.LectureDetail;
+import khu.bigdata.infou.domain.LectureUdemy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LectureDetailRepository extends JpaRepository<LectureDetail, Long> {
+
+    Optional<LectureDetail> findByLectureUdemy(LectureUdemy lectureUdemy);
 }
