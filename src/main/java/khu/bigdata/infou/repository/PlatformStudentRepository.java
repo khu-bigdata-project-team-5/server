ba20e5e7e058c84f813f6bf464d56b9234018d4e
yxhwxn <officeyh9819@gmail.com>
2024-06-13 04:37:06 +0900

    :recycle: Refactor: 쿼리 수정 및 파라미터 타입 변환

--- src/main/java/khu/bigdata/infou/repository/PlatformStudentRepository.java
+++ src/main/java/khu/bigdata/infou/repository/PlatformStudentRepository.java
@@ -1,6 +1,7 @@
 package khu.bigdata.infou.repository;
 
 import khu.bigdata.infou.domain.PlatformStudent;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,14 +9,14 @@ import java.util.List;
 
 public interface PlatformStudentRepository extends JpaRepository<PlatformStudent, Long> {
 
-    @Query("SELECT p.inflearnUserId, COUNT(p) FROM PlatformStudent p WHERE p.inflearnUserId IS NOT NULL GROUP BY p.inflearnUserId ORDER BY COUNT(p) DESC")
-    List<Object[]> countInflearnUserId();
+    @Query("SELECT p.inflearnUserId AS userId, COUNT(p) AS cnt FROM PlatformStudent p WHERE p.inflearnUserId IS NOT NULL GROUP BY p.inflearnUserId " +
+            "UNION " +
+            "SELECT p.udemyUserId AS userId, COUNT(p) AS cnt FROM PlatformStudent p WHERE p.udemyUserId IS NOT NULL GROUP BY p.udemyUserId " +
+            "ORDER BY cnt DESC")
+    List<Object[]> findTopStudents(Pageable pageable);
 
-    @Query("SELECT p.udemyUserId, COUNT(p) FROM PlatformStudent p WHERE p.udemyUserId IS NOT NULL GROUP BY p.udemyUserId ORDER BY COUNT(p) DESC")
-    List<Object[]> countUdemyUserId();
+    List<PlatformStudent> findByInflearnUserId(Integer userId);
 
-    List<PlatformStudent> findByInflearnUserId(Long inflearnUserId);
-
-    List<PlatformStudent> findByUdemyUserId(Long udemyUserId);
+    List<PlatformStudent> findByUdemyUserId(Integer userId);
 }
 
