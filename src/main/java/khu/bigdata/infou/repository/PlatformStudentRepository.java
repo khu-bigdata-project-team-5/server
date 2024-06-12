4ab82957918f441f5d080cbff133465ae9a948bd
yxhwxn <officeyh9819@gmail.com>
2024-06-13 03:35:44 +0900

    :sparkles: Feat: userId의 count 값을 기준으로 수강생 추출

--- src/main/java/khu/bigdata/infou/repository/PlatformStudentRepository.java
+++ src/main/java/khu/bigdata/infou/repository/PlatformStudentRepository.java
@@ -2,6 +2,20 @@ package khu.bigdata.infou.repository;
 
 import khu.bigdata.infou.domain.PlatformStudent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PlatformStudentRepository extends JpaRepository<PlatformStudent, Long> {
+
+    @Query("SELECT p.inflearnUserId, COUNT(p) FROM PlatformStudent p WHERE p.inflearnUserId IS NOT NULL GROUP BY p.inflearnUserId ORDER BY COUNT(p) DESC")
+    List<Object[]> countInflearnUserId();
+
+    @Query("SELECT p.udemyUserId, COUNT(p) FROM PlatformStudent p WHERE p.udemyUserId IS NOT NULL GROUP BY p.udemyUserId ORDER BY COUNT(p) DESC")
+    List<Object[]> countUdemyUserId();
+
+    List<PlatformStudent> findByInflearnUserId(Long inflearnUserId);
+
+    List<PlatformStudent> findByUdemyUserId(Long udemyUserId);
 }
+
